5c0ced305c59f165bebc7a78f7f3b4d15b44bc3e
blacdev <stevenowoborode@gmail.com>
2022-01-29 23:14:05 +0100

    .

--- userbankaccount/urls.py
+++ userbankaccount/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.UserExchangeAPIView, name='user_Exchange'),
-    path("user-info/", views.UserInfoAPIView, name='user_Info'),
+    path("user-info/<int:id>", views.UserInfoAPIView, name='user_Info'),
     # path('', views.Add_List_AccountAPIView.as_view(), name='add account'),
     # path('<int:id>', views.Account_DetailAPIView.as_view(), name='account details'),
 ]
